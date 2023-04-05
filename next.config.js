d0f6ef2cf9efb5e562362ba7852e89a70cf9aa82
Hey Türkiye <110188917+heyturkiye58@users.noreply.github.com>
2023-04-05 22:14:50 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -5,8 +5,8 @@ const withNextra = require('nextra')({
 
 module.exports = withNextra({
     i18n: {
-        locales: ['en'],
-        defaultLocale: 'en',
+        locales: ['tr'],
+        defaultLocale: 'tr',
         localeDetection: true
     }
 })
